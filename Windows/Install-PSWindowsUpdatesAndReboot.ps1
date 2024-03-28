6fe510c46e963ae9f141249926a24a0bb5d52dd4
mellonaut <65114647+mellonaut@users.noreply.github.com>
2024-03-28 06:07:06 -0400

    Create Install-PSWindowsUpdatesAndReboot.ps1

---
+++ Windows/Install-PSWindowsUpdatesAndReboot.ps1
@@ -0,0 +1,4 @@
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force;
+Install-module PSwindowsUpdate -Force;
+powershell -ep bypass Import-Module PSwindowsUpdate -Force;
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ep bypass -c powershell -ep bypass Start-Transcript -Path "~\Desktop\updatelog.txt" -Append; powershell -ep bypass Import-Module PSWindowsUpdate -Force; powershell -ep bypass Get-WindowsUpdate -Install -AcceptAll -Autoreboot -Verbose; powershell -ep bypass Stop-Transcript

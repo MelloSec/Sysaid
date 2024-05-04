76822ae2982b9d7cc4ccfee5ac27d2ca3449188b
mellonaut <65114647+mellonaut@users.noreply.github.com>
2024-05-04 07:39:42 -0400

    Create HTTP-HealthCheck.cs

---
+++ Azure/Functions/HTTP-HealthCheck.cs
@@ -0,0 +1,200 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace biHealthCheck
+{
+    public static class Function1
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        [FunctionName("Function1")]
+        public static async Task RunTimer(
+            [TimerTrigger("0 0 */12 * * *")] TimerInfo myTimer,
+            ILogger log)
+        {
+            string blobUri = Environment.GetEnvironmentVariable("BlobUri");
+            string webhookUrl = Environment.GetEnvironmentVariable("webhookUrl");
+            string connectionString = Environment.GetEnvironmentVariable("AzureWebJobsStorage");
+            string[] ipAddresses = await ReadIpAddressesFromBlobAsync(blobUri, connectionString);
+
+            string imageUrl = "https://blueirissoftware.com/wp-content/uploads/2019/06/splash.jpg";
+
+            bool allOnline = true;
+
+            foreach (var ipAddress in ipAddresses)
+            {
+                int retryCount = 0;
+                bool success = false;
+
+                while (retryCount < 3)
+                {
+                    try
+                    {
+                      // Change to HTTPs if needed
+                      HttpResponseMessage response = await httpClient.GetAsync($"http://{ipAddress}");
+
+                      // Or Use
+                      // Try HTTPS first
+                      // response = await httpClient.GetAsync($"https://{ipAddress}");
+                      // if success, log and set true / else 
+                      //  HttpResponseMessage response = await httpClient.GetAsync($"http://{ipAddress}");
+                      // if success set bool / else         log.LogWarning($"Failed to reach IP address {ipAddress} using both HTTPS and HTTP. Retrying...");
+                      // await Task.Delay(TimeSpan.FromMinutes(3));
+                      // retryCount++;
+                      
+                      
+
+                      
+                        if (response.IsSuccessStatusCode)
+                        {
+                            success = true;
+                            log.LogInformation($"IP address {ipAddress} is online.");
+
+                            break;
+                        }
+                        else
+                        {
+                            log.LogWarning($"Failed to reach IP address {ipAddress}. Retrying...");
+                            await Task.Delay(TimeSpan.FromMinutes(3));
+                            retryCount++;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        log.LogError($"An error occurred while testing IP address {ipAddress}: {ex.Message}");
+                        retryCount++;
+                    }
+                }
+
+                if (!success)
+                {
+                    log.LogError($"Failed to reach IP address {ipAddress} after 3 retries.");
+                    allOnline = false;
+
+                    var adaptiveCardJson = $@"{{
+    ""type"": ""message"",
+    ""attachments"": [
+        {{
+            ""contentType"": ""application/vnd.microsoft.card.adaptive"",
+            ""content"": {{
+                ""type"": ""AdaptiveCard"",
+                ""body"": [
+                    {{
+                        ""type"": ""TextBlock"",
+                        ""text"": ""Service Disrupted - One or More Hosts are Offline"",
+                        ""size"": ""Large"",
+                        ""weight"": ""Bolder"",
+                        ""color"": ""Attention""
+                    }},
+                    {{
+                        ""type"": ""TextBlock"",
+                        ""text"": ""Failed to reach IP address {ipAddress} after 3 retries.""
+                    }},
+                    {{
+                        ""type"": ""Image"",
+                        ""url"": ""{imageUrl}"",
+                        ""size"": ""Medium""
+                    }}
+                ],
+                ""$schema"": ""http://adaptivecards.io/schemas/adaptive-card.json"",
+                ""version"": ""1.0""
+            }}
+        }}
+    ]
+}}";
+
+
+                    var content = new StringContent(adaptiveCardJson, Encoding.UTF8, "application/json");
+                    var response = await httpClient.PostAsync(webhookUrl, content);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        log.LogInformation($"Failed to reach IP address {ipAddress}. Adaptive Card notification posted to webhook.");
+                    }
+                    else
+                    {
+                        log.LogError($"Failed to post Adaptive Card notification to webhook for failed IP address {ipAddress}. Status code: {response.StatusCode}");
+                    }
+                }
+            }
+
+            if (allOnline)
+            {
+                var adaptiveCardJson = $@"{{
+    ""type"": ""message"",
+    ""attachments"": [
+        {{
+            ""contentType"": ""application/vnd.microsoft.card.adaptive"",
+            ""content"": {{
+                ""type"": ""AdaptiveCard"",
+                ""body"": [
+                    {{
+                        ""type"": ""TextBlock"",
+                        ""text"": ""Status Okay - Target Hosts Online"",
+                        ""size"": ""Large"",
+                        ""weight"": ""Bolder"",
+                        ""color"": ""Good""
+                    }},
+                    {{
+                        ""type"": ""TextBlock"",
+                        ""text"": ""All systems are responding to requests.""
+                    }},
+                    {{
+                        ""type"": ""Image"",
+                        ""url"": ""{imageUrl}"",
+                        ""size"": ""Medium""
+                    }}
+                ],
+                ""$schema"": ""http://adaptivecards.io/schemas/adaptive-card.json"",
+                ""version"": ""1.0""
+            }}
+        }}
+    ]
+}}";
+
+                var content = new StringContent(adaptiveCardJson, Encoding.UTF8, "application/json");
+                var response = await httpClient.PostAsync(webhookUrl, content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    log.LogInformation("Adaptive Card message sent successfully.");
+                }
+                else
+                {
+                    log.LogError($"Failed to send Adaptive Card message. Status code: {response.StatusCode}");
+                }
+            }
+        }
+
+        async static Task<string[]> ReadIpAddressesFromBlobAsync(string blobUri, string connectionString)
+        {
+            BlobServiceClient blobServiceClient = new BlobServiceClient(connectionString);
+
+            UriBuilder uriBuilder = new UriBuilder(blobUri);
+            string blobContainerName = uriBuilder.Uri.Segments[1];
+            string blobName = uriBuilder.Uri.Segments[2];
+
+            BlobContainerClient containerClient = blobServiceClient.GetBlobContainerClient(blobContainerName);
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+
+            List<string> ipAddresses = new List<string>();
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                await blobClient.DownloadToAsync(stream);
+                string blobContent = Encoding.UTF8.GetString(stream.ToArray());
+                ipAddresses.AddRange(blobContent.Split(Environment.NewLine));
+            }
+
+            return ipAddresses.ToArray();
+        }
+    }
+}
